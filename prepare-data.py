7b32f552f88b63bf681100e8bdf68b17fc8bc76e
sren <zhiyuanrenusa@gmail.com>
2025-09-07 10:16:05 -0700

    add data prepare code and requirements.txt

---
+++ prepare-data.py
@@ -0,0 +1,10 @@
+import torchaudio
+
+print("Downloading SpeechCommands dataset...")
+
+# This command will download the data to a folder named "SpeechCommands"
+# in your current directory if it's not already there.
+train_dataset = torchaudio.datasets.SPEECHCOMMANDS(root=".", download=True)
+
+print("Download complete!")
+print(f"Number of training samples: {len(train_dataset)}")
